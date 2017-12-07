0c2ac9940bd044df52e9e6d9bad6e2d341ece0a9
marcomendez <marco.mendez@fundacion-jala.org>
2017-12-07 11:46:35 -0400

    Log In to sale force with Api Rest Assured

---
+++ src/main/java/salesforce/salesforceapp/api/Main.java
@@ -0,0 +1,109 @@
+package salesforce.salesforceapp.api;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.IOException;
+
+public class Main {
+    
+  static final String USERNAME     = "marticus123@gmail.com";
+  static final String PASSWORD     = "MM77884632mmYcIUAsjKVfcTCP1iUpBzDiJ7U";
+  static final String LOGINURL     = "https://login.salesforce.com";
+  static final String GRANTSERVICE = "/services/oauth2/token?grant_type=password";
+  static final String CLIENTID     = "3MVG9g9rbsTkKnAWiLjlmeV55qsw.ER1yqbzrACUbniyhN2sgEUyHexclth8zUyp_443K2i.fV9ydgCD9YfMs";
+  static final String CLIENTSECRET = "630723412882675209";
+
+  public static void main(String[] args) {
+
+    DefaultHttpClient httpclient = new DefaultHttpClient();
+
+    // Assemble the login request URL
+    String loginURL = LOGINURL +
+        GRANTSERVICE +
+        "&client_id=" + CLIENTID +
+        "&client_secret=" + CLIENTSECRET +
+        "&username=" + USERNAME +
+        "&password=" + PASSWORD;
+
+    // Login requests must be POSTs
+    HttpPost httpPost = new HttpPost(loginURL);
+
+   HttpResponse response = null;
+
+
+    try {
+      // Execute the login POST request
+      response = httpclient.execute(httpPost);
+    } catch (ClientProtocolException cpException) {
+      // Handle protocol exception
+    } catch (IOException ioException) {
+      // Handle system IO exception
+    }
+
+    // verify response is HTTP OK
+    final int statusCode = response.getStatusLine().getStatusCode();
+    if (statusCode != HttpStatus.SC_OK) {
+      System.out.println("Error authenticating to Force.com: "+statusCode);
+      // Error is in EntityUtils.toString(response.getEntity())
+      return;
+    }
+
+    String getResult = null;
+    try {
+      getResult = EntityUtils.toString(response.getEntity());
+    } catch (IOException ioException) {
+      // Handle system IO exception
+    }
+    JSONObject jsonObject = null;
+    String loginAccessToken = null;
+    String loginInstanceUrl = null;
+    try {
+      jsonObject = (JSONObject) new JSONTokener(getResult).nextValue();
+      loginAccessToken = jsonObject.getString("access_token");
+      loginInstanceUrl = jsonObject.getString("instance_url");
+    } catch (JSONException jsonException) {
+      // Handle JSON exception
+    }
+
+
+
+  //***************************************************
+
+   System.out.println("hola marco"+response.getStatusLine());
+    System.out.println("Successful login");
+    System.out.println("  instance URL: "+loginInstanceUrl);
+    System.out.println("  access token/session ID: "+loginAccessToken);
+
+    // release connection
+    httpPost.releaseConnection();
+
+
+
+    //*********************************************
+  /*  DefaultHttpClient httpclient2 = new DefaultHttpClient();
+    HttpGet get=new HttpGet(loginInstanceUrl);
+    HttpResponse response2 = null;
+    response = httpclient2.execute(get);*/
+    RestAssured.baseURI = loginInstanceUrl+"/services/data/v41.0/sobjects";
+
+    RequestSpecification httpRequest = RestAssured.given();
+
+    Response response2 = httpRequest.request(Method.GET, "/product2");
+
+    String responseBody = response2.getBody().asString();
+    System.out.println("Response Body is =>  " + responseBody);
+
+  }
+}
