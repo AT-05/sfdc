ddb6980a7684f9540995fb713ec5466bf2b139bf
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-12-08 20:08:34 -0400

    Updating OppyContentPage classes

--- src/main/java/salesforce/salesforceapp/ui/opportunities/OppyContentPageClassic.java
+++ src/main/java/salesforce/salesforceapp/ui/opportunities/OppyContentPageClassic.java
@@ -5,4 +5,14 @@ public class OppyContentPageClassic extends OppyContentPage {
   @Override
   public void waitUntilPageObjectIsLoaded() {
   }
+
+  /**
+   * <p>This method sends to Opportunity Quotes View.</p>
+   *
+   * @return a OppyQuotesView object type.
+   */
+  @Override
+  public OppyQuotesView goToQuotesView() {
+    return new OppyQuotesViewClassic();
+  }
 }
