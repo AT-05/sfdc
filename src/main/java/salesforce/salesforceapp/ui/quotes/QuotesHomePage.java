1ec872e6ed7c95dbd3dd495090c8ab5791efc577
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-12-10 21:59:12 -0400

    Updating QuotesHomePage classes

--- src/main/java/salesforce/salesforceapp/ui/quotes/QuotesHomePage.java
+++ src/main/java/salesforce/salesforceapp/ui/quotes/QuotesHomePage.java
@@ -1,8 +1,6 @@
 package salesforce.salesforceapp.ui.quotes;
 
-import org.openqa.selenium.*;
 import salesforce.salesforceapp.ui.HomeBasePage;
-import salesforce.salesforceapp.ui.PageFactory;
 
 /**
  * Created by Franco Aldunate on 12/5/2017.
@@ -14,9 +12,14 @@ public abstract class QuotesHomePage extends HomeBasePage {
    * @param quoteName is the quote name given.
    * @return a QuotesContentPage object type.
    */
-  public QuotesContentPage selectQuote(String quoteName){
-    WebElement element = driver.findElement(By.xpath("//a[contains(@title,'" + quoteName + "')]"));
-    driverTools.clickElement(element);
-    return PageFactory.getQuotesContentPage();
-  }
+  public abstract QuotesContentPage selectQuote(String quoteName);
+
+  /**
+   * <p>This method checks existence of Quote element
+   * in the quote's list.</p>
+   *
+   * @param quoteName is the given quote name.
+   * @return whether the quote exists in the list or not.
+   */
+  public abstract boolean isQuoteElementPresent(String quoteName);
 }
--- src/main/java/salesforce/salesforceapp/ui/quotes/QuotesHomePageClassic.java
+++ src/main/java/salesforce/salesforceapp/ui/quotes/QuotesHomePageClassic.java
@@ -1,5 +1,8 @@
 package salesforce.salesforceapp.ui.quotes;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by Franco Aldunate on 12/5/2017.
  */
@@ -7,5 +10,31 @@ public class QuotesHomePageClassic extends QuotesHomePage {
   @Override
   public void waitUntilPageObjectIsLoaded() {
 
+  }
+
+  /**
+   * <p>This method sends to a quote's content page.</p>
+   *
+   * @param quoteName is the quote name given.
+   * @return a QuotesContentPage object type.
+   */
+  @Override
+  public QuotesContentPage selectQuote(String quoteName) {
+    WebElement element = driver.findElement(By.xpath("//a[text()='" + quoteName + "']"));
+    driverTools.clickElement(element);
+    return new QuotesContentPageClassic();
+  }
+
+  /**
+   * <p>This method checks existence of Quote element
+   * in the quote's list.</p>
+   *
+   * @param quoteName is the given quote name.
+   * @return whether the quote exists in the list or not.
+   */
+  @Override
+  public boolean isQuoteElementPresent(String quoteName) {
+    WebElement element = driver.findElement(By.xpath("//a[text()='" + quoteName + "']"));
+    return element.isDisplayed();
   }
 }
