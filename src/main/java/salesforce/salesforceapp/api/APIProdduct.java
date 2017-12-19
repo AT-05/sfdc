b852a5943fc6f9e9af8c7b6c63792c40f153a75a
marcomendez <marco.mendez@fundacion-jala.org>
2017-12-19 19:13:49 -0400

    crud api

--- src/main/java/salesforce/salesforceapp/api/APIProdduct.java
+++ src/main/java/salesforce/salesforceapp/api/APIProdduct.java
@@ -1,41 +1,91 @@
 package salesforce.salesforceapp.api;
 
+import com.sun.jndi.toolkit.url.Uri;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+import static org.junit.Assert.assertTrue;
+import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class APIProdduct {
     CoreAPI coreAPI = new CoreAPI();
 
+
     @Test
     public void SuperPostProduct() throws URISyntaxException {
-        String myJson = "{\"Name\":\"New product created from api2\"}";
+        String myJson = "{\n" +
+                "\t\"Name\": \"marco11\",\n" +
+                "\"ProductCode\":\"hola\"\n" +
+                "}";
+        setUp();
         String request = "https://na73.salesforce.com/services/data/v41.0/sobjects/product2";
-
-  /*      coreAPI.setUserName("marco@monkeysoft.com");
-        coreAPI.setPassword("MM77884632mmkRCe5cLFOk6hVuSTWxcElCEL");// password + security token
-        coreAPI.setClientId("3MVG9g9rbsTkKnAWV1Lk6731UUUtxGnMmmVO87mFYpeXjRkiZkSkEytJGBGmckIAw2Rh6.E3aX7YBq_KchUOK");
-        coreAPI.setClientSecret("643257347336618396");*/
-
-        coreAPI.setUserName("marco.mendez@fundacion-jala.org");
-        coreAPI.setPassword("MM77884632mm7Bz6JjPI3g0CqBt7DTjNpuNpD");// password + token
-        coreAPI.setClientId("3MVG9mclR62wycM2MRo29iLsfd_lw6ep5Lb11mRDrMyMvDwrNMoSmmZc88D9.bINLkzEjCX1OVLZyPy7Ulmf2");
-        coreAPI.setClientSecret("1931487572682630595");
 
         Response post = given()
                 .contentType("application/json")
                 .headers("Authorization", "OAuth ".concat(coreAPI.getAccessToken()))
                 .body(myJson)
-                .when()
                 .post(request);
 
         String body = post.getBody().asString();
         System.out.println(body);
+
+    }
+
+    public void setUp() {
+        coreAPI.setUserName("marco@monkeysoft.com");
+        coreAPI.setPassword("MM77884632mmkRCe5cLFOk6hVuSTWxcElCEL");// password + security token
+        coreAPI.setClientId("3MVG9g9rbsTkKnAWV1Lk6731UUUtxGnMmmVO87mFYpeXjRkiZkSkEytJGBGmckIAw2Rh6.E3aX7YBq_KchUOK");
+        coreAPI.setClientSecret("643257347336618396");
+    }
+
+
+    @Test
+    public void getProduct() throws URISyntaxException {
+        setUp();
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .headers("Authorization", "OAuth ".concat(coreAPI.getAccessToken()))
+                .params("q", "SELECT name from product2 where name='marco11'")
+                .when()
+                .get("https://na73.salesforce.com/services/data/v20.0/query");
+        System.out.println(response.getBody().asString());
+
+        JsonPath jsonPath = new JsonPath(response.asString());
+        String name = jsonPath.getString("records");
+        System.out.println(name.contains("Name:marco11"));
+
+    }
+
+    @Test
+    public void deleteProduct() throws URISyntaxException {
+        setUp();
+        given().contentType(ContentType.JSON)
+                .headers("Authorization", "OAuth ".concat(coreAPI.getAccessToken()))
+                .when()
+                .delete("https://na73.salesforce.com/services/data/v20.0/sobjects/Product2/01t1I000001ZeVyQAK");
+
+        Response delete = given()
+                .contentType(ContentType.JSON)
+                .headers("Authorization", "OAuth ".concat(coreAPI.getAccessToken()))
+                .when()
+                .delete("https://na73.salesforce.com/services/data/v20.0/sobjects/Product2/01t1I000001ZeVyQAK");
+        System.out.println(delete.getBody().asString());
+        JsonPath jsonPath = new JsonPath(delete.asString());
+        String name = jsonPath.getString("message");
+        System.out.println("It deleted :" + name.contains("entity is deleted"));
 
 
     }
--- src/main/java/salesforce/salesforceapp/api/Main.java
+++ src/main/java/salesforce/salesforceapp/api/Main.java
@@ -17,92 +17,90 @@ import org.json.JSONTokener;
 import java.io.IOException;
 
 public class Main {
-    
-  static final String USERNAME     = "marticus123@gmail.com";
-  static final String PASSWORD     = "MM77884632mmYcIUAsjKVfcTCP1iUpBzDiJ7U";
-  static final String LOGINURL     = "https://login.salesforce.com";
-  static final String GRANTSERVICE = "/services/oauth2/token?grant_type=password";
-  static final String CLIENTID     = "3MVG9g9rbsTkKnAWiLjlmeV55qsw.ER1yqbzrACUbniyhN2sgEUyHexclth8zUyp_443K2i.fV9ydgCD9YfMs";
-  static final String CLIENTSECRET = "630723412882675209";
 
-  public static void main(String[] args) {
+    static final String USERNAME = "marco@monkeysoft.com";
+    static final String PASSWORD = "MM77884632mmkRCe5cLFOk6hVuSTWxcElCEL";
+    static final String LOGINURL = "https://login.salesforce.com";
+    static final String GRANTSERVICE = "/services/oauth2/token?grant_type=password";
+    static final String CLIENTID = "3MVG9g9rbsTkKnAWV1Lk6731UUUtxGnMmmVO87mFYpeXjRkiZkSkEytJGBGmckIAw2Rh6.E3aX7YBq_KchUOK";
+    static final String CLIENTSECRET = "643257347336618396";
 
-    DefaultHttpClient httpclient = new DefaultHttpClient();
+    public static void main(String[] args) {
 
-    // Assemble the login request URL
-    String loginURL = LOGINURL +
-        GRANTSERVICE +
-        "&client_id=" + CLIENTID +
-        "&client_secret=" + CLIENTSECRET +
-        "&username=" + USERNAME +
-        "&password=" + PASSWORD;
+        DefaultHttpClient httpclient = new DefaultHttpClient();
 
-    // Login requests must be POSTs
-    HttpPost httpPost = new HttpPost(loginURL);
+        // Assemble the login request URL
+        String loginURL = LOGINURL +
+                GRANTSERVICE +
+                "&client_id=" + CLIENTID +
+                "&client_secret=" + CLIENTSECRET +
+                "&username=" + USERNAME +
+                "&password=" + PASSWORD;
 
-   HttpResponse response = null;
+        // Login requests must be POSTs
+        HttpPost httpPost = new HttpPost(loginURL);
 
-
-    try {
-      // Execute the login POST request
-      response = httpclient.execute(httpPost);
-    } catch (ClientProtocolException cpException) {
-      // Handle protocol exception
-    } catch (IOException ioException) {
-      // Handle system IO exception
-    }
-
-    // verify response is HTTP OK
-    final int statusCode = response.getStatusLine().getStatusCode();
-    if (statusCode != HttpStatus.SC_OK) {
-      System.out.println("Error authenticating to Force.com: "+statusCode);
-      // Error is in EntityUtils.toString(response.getEntity())
-      return;
-    }
-
-    String getResult = null;
-    try {
-      getResult = EntityUtils.toString(response.getEntity());
-    } catch (IOException ioException) {
-      // Handle system IO exception
-    }
-    JSONObject jsonObject = null;
-    String loginAccessToken = null;
-    String loginInstanceUrl = null;
-    try {
-      jsonObject = (JSONObject) new JSONTokener(getResult).nextValue();
-      loginAccessToken = jsonObject.getString("access_token");
-      loginInstanceUrl = jsonObject.getString("instance_url");
-    } catch (JSONException jsonException) {
-      // Handle JSON exception
-    }
+        HttpResponse response = null;
 
 
+        try {
+            // Execute the login POST request
+            response = httpclient.execute(httpPost);
+        } catch (ClientProtocolException cpException) {
+            // Handle protocol exception
+        } catch (IOException ioException) {
+            // Handle system IO exception
+        }
 
-  //***************************************************
+        // verify response is HTTP OK
+        final int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode != HttpStatus.SC_OK) {
+            System.out.println("Error authenticating to Force.com: " + statusCode);
+            // Error is in EntityUtils.toString(response.getEntity())
+            return;
+        }
 
-    System.out.println("Successful login");
-    System.out.println("  instance URL: "+loginInstanceUrl);
-    System.out.println("  access token/session ID: "+loginAccessToken);
+        String getResult = null;
+        try {
+            getResult = EntityUtils.toString(response.getEntity());
+        } catch (IOException ioException) {
+            // Handle system IO exception
+        }
+        JSONObject jsonObject = null;
+        String loginAccessToken = null;
+        String loginInstanceUrl = null;
+        try {
+            jsonObject = (JSONObject) new JSONTokener(getResult).nextValue();
+            loginAccessToken = jsonObject.getString("access_token");
+            loginInstanceUrl = jsonObject.getString("instance_url");
+        } catch (JSONException jsonException) {
+            // Handle JSON exception
+        }
 
-    // release connection
-    httpPost.releaseConnection();
+
+        //***************************************************
+
+        System.out.println("Successful login");
+        System.out.println("  instance URL: " + loginInstanceUrl);
+        System.out.println("  access token/session ID: " + loginAccessToken);
+
+        // release connection
+        httpPost.releaseConnection();
 
 
-
-    //*********************************************
+        //*********************************************
   /*  DefaultHttpClient httpclient2 = new DefaultHttpClient();
     HttpGet get=new HttpGet(loginInstanceUrl);
     HttpResponse response2 = null;
     response = httpclient2.execute(get);*/
-    RestAssured.baseURI = loginInstanceUrl+"/services/data/v41.0/sobjects";
+        RestAssured.baseURI = loginInstanceUrl + "/services/data/v41.0/sobjects";
 
-    RequestSpecification httpRequest = RestAssured.given();
+        RequestSpecification httpRequest = RestAssured.given();
 
-    Response response2 = httpRequest.request(Method.GET, "/product2");
+        Response response2 = httpRequest.request(Method.GET, "/product2");
 
-    String responseBody = response2.getBody().asString();
-    System.out.println("Response Body is =>  " + responseBody);
+        String responseBody = response2.getBody().asString();
+        System.out.println("Response Body is =>  " + responseBody);
 
-  }
+    }
 }
