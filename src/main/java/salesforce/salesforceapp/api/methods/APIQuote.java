4194908d635dbf500bf66002ac853fc4c02e05d9
Franco Daniel Aldunate Cordero <Franco.Aldunate@fundacion-jala.org>
2017-12-21 11:20:01 -0400

    Adding api base

--- src/main/java/salesforce/salesforceapp/api/methods/APIQuote.java
+++ src/main/java/salesforce/salesforceapp/api/methods/APIQuote.java
@@ -13,67 +13,35 @@ import salesforce.salesforceapp.entities.quotes.Quote;
 /**
  * Created by Franco Aldunate on 12/17/2017.
  */
-public class APIQuote {
-  private final String QUERY = "/query";
+public class APIQuote extends APIBase {
   private final Quote quote;
-  private static final APIManager apiManager = APIManager.getInstance();
-  private Map<String, Object> quoteFieldsMap;
 
   public APIQuote(Quote quote) {
-    quoteFieldsMap = new HashMap<>();
     this.quote = quote;
-    quoteFieldsMap = covertQuoteToMap(quote);
+    fieldsMap = covertQuoteToMap();
+    apiObject = QUOTE;
   }
 
-  public boolean isQuoteSaved() {
-    System.out.println(apiManager.getQuery(QUERY, QUOTE, quoteFieldsMap).asString());
-    String totalSize = (apiManager.getQuery(QUERY, QUOTE, quoteFieldsMap).jsonPath().get("totalSize")).toString();
-    System.out.println("total size: " + totalSize);
-    return Integer.parseInt(totalSize) > 0;
-  }
-
-  public void createQuote() {
-    String endPoint = SOBJECTS + QUOTE + "/";
-    System.out.println("*******creating quote");
-    Response responseAccount = APIManager.getInstance().post(endPoint, quoteFieldsMap);
-    System.out.println("*******response" + responseAccount.asString());
-    quote.setId(responseAccount.jsonPath().get("id"));
-    System.out.println("********create quote id: " + quote.getId());
-  }
-
-  public void deleteQuote() {
-    System.out.println("********delete quote id: " + quote.getId());
-    String endPoint = SOBJECTS + QUOTE + "/" + quote.getId();
-    System.out.println("*******deleting quote");
-    Response responseAccount = APIManager.getInstance().delete(endPoint);
-    System.out.println("*******response" + responseAccount.asString());
-  }
-
-  /**
-   * <p>This method converts the </p>
-   *
-   * @param quote
-   * @return
-   */
-  public Map<String, Object> covertQuoteToMap(Quote quote) {
+  @Override
+  protected Map<String, Object> covertQuoteToMap() {
     Map<String, Object> quoteMap = new HashMap<>();
     quoteMap.put("Name", quote.getName());
-    quoteMap.put("OpportunityIde", quote.getOpportunityId());
+    quoteMap.put("OpportunityId", quote.getOpportunityId());
     quoteMap.put("ExpirationDate", DateConverter.convertDateFormat(quote.getExpirationDate()));
     quoteMap.put("Status", quote.getStatus());
     quoteMap.put("Description", quote.getDescription());
-    quoteMap.put("Tax", quote.getTax());
-    quoteMap.put("ShippingHandling", quote.getShippingAndHandling());
+    quoteMap.put("Tax", quote.getTaxAsDouble());
+    quoteMap.put("ShippingHandling", quote.getShippingAndHandlingAsDouble());
     return quoteMap;
   }
 
-  /**
-   * <p>This mehtod gets quote id provided in creating
-   * quote api response.</p>
-   *
-   * @return the quote api id.
-   */
-  public String getQuoteId() {
+  @Override
+  protected void setAPIObjectId() {
+    quote.setId(response.jsonPath().get(ID));
+  }
+
+  @Override
+  protected String gettAPIObjectId() {
     return quote.getId();
   }
 }
